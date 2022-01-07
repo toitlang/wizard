f2f3028f578bfd4306f1a1ff09b57fb69b65c5ff
jstroem <jstroem@users.noreply.github.com>
2022-01-07 11:16:37 +0000

    Deploying to gh-pages from @ toitlang/wizard@d9ae336e918b77a6cc902807a997ed37f0a09a63 🚀

---
+++ precache-manifest.80aec4dbce03ea96bebf8fe668cb9417.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d33af12e45253a4ddb0111443e6077",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcdb5492a33d2fbd5443",
+    "url": "/static/css/main.d789aa55.chunk.css"
+  },
+  {
+    "revision": "fd95273289429ae80033",
+    "url": "/static/js/2.486e6110.chunk.js"
+  },
+  {
+    "revision": "bcdb5492a33d2fbd5443",
+    "url": "/static/js/main.32651a8b.chunk.js"
+  },
+  {
+    "revision": "9565078d8fc9ebad4a8c",
+    "url": "/static/js/runtime-main.f3c8cb09.js"
+  },
+  {
+    "revision": "443709faaf753d82d1b04fae558ac4a5",
+    "url": "/static/media/arrow.443709fa.svg"
+  },
+  {
+    "revision": "ceddcfbb6062c45575be73990094cee8",
+    "url": "/static/media/connect_focus.ceddcfbb.svg"
+  },
+  {
+    "revision": "b62db6efb385900bf0a98065722d4211",
+    "url": "/static/media/done.b62db6ef.svg"
+  },
+  {
+    "revision": "214c15c2749e8369f3f0700ac13c337e",
+    "url": "/static/media/provision.214c15c2.svg"
+  },
+  {
+    "revision": "92b0cb45dfdd5ec7f33b01a367f07fe6",
+    "url": "/static/media/provision_focus.92b0cb45.svg"
+  },
+  {
+    "revision": "9108c758cd8486e13e88d5c30c16ab74",
+    "url": "/static/media/suggestion.9108c758.svg"
+  },
+  {
+    "revision": "029971c17052ade6b641e77f4a9adf54",
+    "url": "/static/media/wifi.029971c1.svg"
+  },
+  {
+    "revision": "90d83e341449b656ee72d4b09987f453",
+    "url": "/static/media/wifi_focus.90d83e34.svg"
+  }
+]);
