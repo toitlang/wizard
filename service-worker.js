163caf279d61298e8a7b988255f3644b17155816
jstroem <jstroem@users.noreply.github.com>
2022-01-10 09:28:36 +0000

    Deploying to gh-pages from @ toitlang/wizard@d711b78e59cc4ad817d7c1c91dc40a044f400662 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.80aec4dbce03ea96bebf8fe668cb9417.js"
+  "/wizard/precache-manifest.9d50f0bb8f8349bbe053e4ef9aa076be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wizard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
